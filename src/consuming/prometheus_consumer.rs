4b68f0358614f9886dfad4457196ce2987be632e
Eugene Butusov <evgenybutusov@gmail.com>
2021-12-22 23:34:15 +0300

    Add consuming for Prometheus consumer

--- src/consuming/prometheus_consumer.rs
+++ src/consuming/prometheus_consumer.rs
@@ -1,32 +1,46 @@
+use std::collections::HashMap;
 use prometheus::{Registry, Gauge, Opts, TextEncoder, Encoder};
 use crate::consuming::common::PullseConsumer;
 use crate::PullseLedger;
 
 pub struct PrometheusConsumer {
     registry: Registry,
+    collectors: HashMap<String, Gauge>,
 }
 
 impl PrometheusConsumer {
     pub fn new(ledger: &PullseLedger) -> PrometheusConsumer {
         let registry = Registry::new();
+        let mut collectors = HashMap::new();
 
         for metric_name in ledger.get_metric_names() {
             let gauge_opts = Opts::new(metric_name, metric_name);
-            let counter = Gauge::with_opts(gauge_opts).unwrap();
+            let gauge = Gauge::with_opts(gauge_opts).unwrap();
 
-            registry.register(Box::new(counter.clone())).unwrap();
+            registry.register(Box::new(gauge.clone())).unwrap();
+            collectors.insert(String::from(metric_name), gauge);
         }
-        PrometheusConsumer { registry }
+        PrometheusConsumer { registry, collectors }
     }
-}
 
-impl PullseConsumer for PrometheusConsumer {
-    fn consume(&self, ledger: &PullseLedger) {
+    pub fn report(&self) {
         let mut buffer = vec![];
         let encoder = TextEncoder::new();
         let metric_families = self.registry.gather();
         encoder.encode(&metric_families, &mut buffer).unwrap();
         let result = String::from_utf8(buffer).unwrap();
         println!("{}", result);
+    }
+}
+
+impl PullseConsumer for PrometheusConsumer {
+    fn consume(&self, ledger: &PullseLedger) {
+        for metric_name in ledger.get_metric_names() {
+            if let Some(collector) = self.collectors.get(metric_name) {
+                if let Some(value) = ledger.raw_data.get(metric_name) {
+                    collector.set(value.clone().into());
+                }
+            }
+        }
     }
 }
